979dd27854c333209dbc56caa1401119997785b9
mirenguerra <miren.guerra.sanz@gmail.com>
2019-04-22 20:46:09 +0200

    Get value from favourites

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,26 +1,26 @@
 "use strict";
 
 const buttonEl = document.querySelector(".search-btn");
+const listEl = document.querySelector('.list');
 const myFavouriteShows = [];
-const favouriteShowEl = {
-  Title: "",
-  Preview: ""
-};
+// const aFavouriteShow (title,preview) ={
+//   Title:'title',
+//   Preview:'preview'
+// }
 
 function getShow() {
+  listEl.innerHTML ='';
   let inputEl = document.querySelector(".search-input");
   fetch(`http://api.tvmaze.com/search/shows?q=${inputEl.value}`)
     .then(showResponse => showResponse.json())
     .then(showData => {
-      document.body.innerHTML += `<ul class="list"></ul>`;
-      const listEl = document.querySelector(".list");
       for (const item of showData) {
         const showTitle = item.show.name;
         if (item.show.image) {
           const showPhoto = item.show.image.medium;
-          listEl.innerHTML += `<li class='element'><h2>${showTitle}</h2><img src="${showPhoto}"/></li>`;
+          listEl.innerHTML += `<li class='element'><h2 class="titleName">${showTitle}</h2><img src="${showPhoto}"/></li>`;
         } else {
-          listEl.innerHTML += `<li class='element'><h2>${showTitle}</h2><img src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV"/></li>`;
+          listEl.innerHTML += `<li class='element'><h2 class="titleName">${showTitle}</h2><img src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV"/></li>`;
         }
         const elementEl = document.querySelectorAll(".element");
         for (let j = 0; j < elementEl.length; j++) {
@@ -38,8 +38,13 @@ buttonEl.addEventListener("click", getShow);
 
 function addToMyFavourites(event) {
   event.currentTarget.classList.add("favouriteShows");
+  // recoger los valores de title e image para guardarlos en el objeto
+  const title = event.currentTarget.children[0].innerHTML;
+  const preview = event.currentTarget.children[1].currentSrc;
 }
+
+
 // Cuando escuchemos el click·
 // guardar esa serie en el objeto
 // guardar el objeto en el array
-// añadir la clase que cambia de color
+
