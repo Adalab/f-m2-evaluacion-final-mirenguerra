9c57ceed4c9f5c0947a408fd9bd2a54ee1193795
mirenguerra <miren.guerra.sanz@gmail.com>
2019-04-22 17:03:17 +0200

    Fix getShow funtion

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -3,18 +3,31 @@
 const inputEl = document.querySelector(".search-input");
 const buttonEl = document.querySelector(".search-btn");
 
-// buttonEl.addEventListener("click", getSeries);
+buttonEl.addEventListener("click", getShow);
 
 function getShow() {
-  fetch("http://api.tvmaze.com/shows")
+  fetch(`http://api.tvmaze.com/search/shows?q=${inputEl.value}`)
     .then(showResponse => showResponse.json())
     .then(showData => {
-        document.body.innerHTML += `<ul class="list"></ul>`;
-        const listEl = document.querySelector('.list');
-      for (const show of showData) {
-        listEl.innerHTML += `<li class='element'>${show.name}</li>`
+      console.log(showData);
+      document.body.innerHTML += `<ul class="list"></ul>`;
+      const listEl = document.querySelector(".list");
+      for (const item of showData) {
+          const serie = item.show.name;
+          // console.log(serie);  
+        listEl.innerHTML += `<li class='element'>${serie}</li>`;
       }
     });
 }
 
-getShow();
+// function getShow() {
+//     fetch("http://api.tvmaze.com/shows")
+//       .then(showResponse => showResponse.json())
+//       .then(showData => {
+//           document.body.innerHTML += `<ul class="list"></ul>`;
+//           const listEl = document.querySelector('.list');
+//         for (const show of showData) {
+//           listEl.innerHTML += `<li class='element'>${show.name}</li>`
+//         }
+//       });
+//   }
