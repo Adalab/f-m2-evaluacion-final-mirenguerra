628aefc117e9621eaf9def0dd6902d50a52d853a
mirenguerra <miren.guerra.sanz@gmail.com>
2019-04-22 16:01:43 +0200

    Add js guide and create getShow function (not finish yet)

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,20 @@
-'use strict';
+"use strict";
 
-console.log('>> Ready :)');
+const inputEl = document.querySelector(".search-input");
+const buttonEl = document.querySelector(".search-btn");
+
+// buttonEl.addEventListener("click", getSeries);
+
+function getShow() {
+  fetch("http://api.tvmaze.com/shows")
+    .then(showResponse => showResponse.json())
+    .then(showData => {
+        document.body.innerHTML += `<ul class="list"></ul>`;
+        const listEl = document.querySelector('.list');
+      for (const show of showData) {
+        listEl.innerHTML += `<li class='element'>${show.name}</li>`
+      }
+    });
+}
+
+getShow();
