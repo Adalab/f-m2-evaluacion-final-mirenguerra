7d37546445f426e5675ee1ae8ba6678e4f5b1c15
mirenguerra <miren.guerra.sanz@gmail.com>
2019-04-22 19:29:10 +0200

    Add js guide and create addToFavourites

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,15 +1,17 @@
 "use strict";
 
-const inputEl = document.querySelector(".search-input");
 const buttonEl = document.querySelector(".search-btn");
-
-buttonEl.addEventListener("click", getShow);
+const myFavouriteShows = [];
+const favouriteShowEl = {
+  Title: "",
+  Preview: ""
+};
 
 function getShow() {
+  let inputEl = document.querySelector(".search-input");
   fetch(`http://api.tvmaze.com/search/shows?q=${inputEl.value}`)
     .then(showResponse => showResponse.json())
     .then(showData => {
-      console.log(showData);
       document.body.innerHTML += `<ul class="list"></ul>`;
       const listEl = document.querySelector(".list");
       for (const item of showData) {
@@ -20,6 +22,24 @@ function getShow() {
         } else {
           listEl.innerHTML += `<li class='element'><h2>${showTitle}</h2><img src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV"/></li>`;
         }
+        const elementEl = document.querySelectorAll(".element");
+        for (let j = 0; j < elementEl.length; j++) {
+          elementEl[j].addEventListener("click", addToMyFavourites);
+        }
       }
     });
 }
+
+buttonEl.addEventListener("click", getShow);
+
+// Crear array vacía para meter favoritos
+// Crear objeto vacío con propiedades Title y Preview. Los objetos se acumularán dentro del array
+// Poner listener en el li de cada serie para escuchar su click
+
+function addToMyFavourites(event) {
+  event.currentTarget.classList.add("favouriteShows");
+}
+// Cuando escuchemos el click·
+// guardar esa serie en el objeto
+// guardar el objeto en el array
+// añadir la clase que cambia de color
